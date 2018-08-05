3ce493f950e10a879bc65fa16664a0baf64b1721
“Brieanna <bmann@elitedigital.cagit config --global user.email bmann@elitedigital.ca>
2018-08-05 18:49:56 -0400

    project page update

--- wp-content/themes/blankslate/index.php
+++ wp-content/themes/blankslate/index.php
@@ -1,11 +1,65 @@
 <?php get_header(); ?>
 <h1>index.php</h1>
-<section id="content" role="main">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php comments_template(); ?>
-<?php endwhile; endif; ?>
-<?php get_template_part( 'nav', 'below' ); ?>
+<section class="white-bkg lg-margin-bottom">
+ <?php echo do_shortcode('[rev_slider alias="header_slider"]'); ?>
 </section>
-<?php get_sidebar(); ?>
+<!-- Case study section -->
+<section class="white-bkg lg-margin-bottom">
+<div class="med-padding" style="background: url('<?php echo get_template_directory_uri().'/assets/images/triangle dots.png'?>')">
+  <div class="sm-padding">
+  <div class="flex flex-wrap space-between  med-padding-bottom m-direction-column">
+    <div class="case-study-img flex direction-column justify-center med-margin m-width-100 ">
+     <h1><?php the_field('case_study_title'); ?></h1>
+     <p class="med-padding-bottom"><?php the_field('case_study_desc'); ?></p>
+   </div>
+   <div class="case-study-img height-32 object-cover med-padding-bottom m-width-100">
+     <a href="<?php the_field('image_1_link'); ?>"><img src="<?php the_field('case_study_image_1'); ?>" class=" object-cover med-padding-bottom m-width-100"></a>
+   </div>
+   <div class="case-study-img height-32 object-cover med-padding-bottom m-width-100">
+        <a href="<?php the_field('image_2_link'); ?>"><img src="<?php the_field('case_study_image_2'); ?>" class=" object-cover med-padding-bottom m-width-100"></a>
+    </div>
+    <div class="case-study-img height-32 object-cover med-padding-bottom m-width-100">
+         <a href="<?php the_field('image_3_link'); ?>"><img src="<?php the_field('case_study_image_3'); ?>" class=" object-cover med-padding-bottom m-width-100"></a>
+     </div>
+     <div class="case-study-img height-32 object-cover med-padding-bottom m-width-100">
+          <a href="<?php the_field('image_4_link'); ?>"><img src="<?php the_field('case_study_image_4'); ?>" class=" object-cover med-padding-bottom m-width-100"></a>
+      </div>
+      <div class="case-study-img height-32 object-cover med-padding-bottom m-width-100">
+           <a href="<?php the_field('image_5_link'); ?>"><img src="<?php the_field('case_study_image_5'); ?>" class=" object-cover med-padding-bottom m-width-100"></a>
+       </div>
+       <div class="case-study-img height-32 object-cover med-padding-bottom m-width-100">
+            <a href="<?php the_field('image_6_link'); ?>"><img src="<?php the_field('case_study_image_6'); ?>" class=" object-cover med-padding-bottom m-width-100"></a>
+        </div>
+        <div class="case-study-img height-32 object-cover med-padding-bottom m-width-100">
+             <a href="<?php the_field('image_7_link'); ?>"><img src="<?php the_field('case_study_image_7'); ?>" class=" object-cover med-padding-bottom m-width-100"></a>
+         </div>
+         <div class="case-study-img height-32 object-cover med-padding-bottom m-width-100">
+              <a href="<?php the_field('image_8_link'); ?>"><img src="<?php the_field('case_study_image_8'); ?>" class=" object-cover med-padding-bottom m-width-100"></a>
+          </div>
+
+</div>
+</div>
+</div>
+</section>
+<!-- End of Case study section -->
+<!-- Start of Form -->
+<section class="white-bkg lg-margin-bottom">
+<div class="med-padding flex m-direction-column" style="background: url('<?php echo get_template_directory_uri().'/assets/images/triangle dots.png'?>')">
+  <div class="width-50 flex justify-center direction-column med-padding">
+    <h1 class="sm-padding-bottom">Contact</h1>
+    <p class="med-padding-bottom">Like what you see? Holla at your girl.</p>
+    <p>519-282-6185<br>
+    <a class="email-link" href="mailto:juliesrvc@gmail.com">juliesrvc@gmail.com</a></p>
+    <div class="flex">
+      <i class="fab fa-instagram grey-font med-padding-top med-padding-right"></i>
+      <i class="fab fa-twitter grey-font med-padding"></i>
+      <i class="fab fa-linkedin grey-font med-padding"></i></div>
+
+  </div>
+  <div class="width-50">
+    <?php   echo do_shortcode( '[contact-form-7 id="5" title="Contact Me"]' );?>
+  </div>
+</div>
+</section>
+<!-- End of Form -->
 <?php get_footer(); ?>
--- wp-content/themes/blankslate/single.php
+++ wp-content/themes/blankslate/single.php
@@ -7,98 +7,143 @@
 </div>
 </section>
 <!-- End Of Project slider -->
-<!-- Bio section -->
-<section class="white-bkg lg-margin-bottom" >
-<div class="xlg-padding">
-  <div class="flex ">
-    <div class="width-30 flex direction-column justify-center ">
+
+
+<!-- project info section -->
+<section class="white-bkg lg-margin-bottom">
+<div class="project-padding">
+  <div class="sm-padding">
+  <div class="flex flex-wrap space-between m-direction-column">
+    <div class="width-32 med-padding-right justify-between flex direction-column m-width-100">
       <div>
-      <h1 class="lg-padding-bottom"><?php the_field('bio_and_credentials_title'); ?></h1>
-      </div>
-      <div>
-      <p class="bold-font"><?php the_field('bio_cred_title_1'); ?></p>
-      <p class="med-padding-bottom"><?php the_field('bio_cred_text_1'); ?></p>
-      <p class="bold-font"><?php the_field('bio_cred_title_2'); ?></p>
-      <p class="med-padding-bottom"><?php the_field('bio_cred_text_2'); ?></p>
-      <p class="bold-font"><?php the_field('bio_cred_title_3'); ?></p>
-      <p class="med-padding-bottom"><?php the_field('bio_cred_text_3'); ?></p>
+       <h1><?php the_field('study_title'); ?></h1>
+       <p><?php the_field('title_sub_title'); ?></p>
+     </div>
+     <div>
+       <p class="bold-font"><?php the_field('my_role_title'); ?></p>
+       <p><?php the_field('role_desc'); ?></p>
+       <p class="bold-font med-padding-top"><?php the_field('tools_title'); ?></p>
+       <p><?php the_field('tools_desc'); ?></p>
     </div>
+   </div>
+    <div class="width-60 m-width-100">
+      <p><?php the_field('project_desc'); ?></p>
     </div>
-    <div class="width-70 lg-padding-left">
-      <p class="med-padding-bottom"><?php the_field('bio_cred_desc'); ?></p>
-      <a href="#" class="resume-btn black-link black-btn-border"> View Resume </a>
+</div>
+</div>
+</div>
+</section>
+<!-- End of project info section -->
+<!-- Work flow slider -->
+<section class="white-bkg lg-margin-bottom">
+  <div class="about-padding flex flex-wrap" style="background: url('<?php echo get_template_directory_uri().'/assets/images/Oval BG.png'?>')">
+    <!-- ROW 1 -->
+  <div class="width-25 m-width-50 sm-width-100">
+    <img src="<?php the_field('user_image'); ?>" width="100%;">
+    <div class="green-bkg">
+      <p class="white-font bold-font"><?php the_field('user_info'); ?></p>
+    </div>
+  </div>
+
+
+    <!-- ROW 2 -->
+    <div class="width-25 m-width-50 sm-width-100">
+      <div class="med-padding-left">
+      <!-- Biography -->
+    <div>
+      <img src="<?php the_field('bio_icon'); ?>">
+      <h5><?php the_field('biography_title'); ?></h5>
+      <div><?php the_field('biography_desc'); ?></div>
+    </div>
+    <!-- Motivations -->
+    <div>
+      <img src="<?php the_field('motivation_icon'); ?>">
+      <h5><?php the_field('motivation_title'); ?></h5>
+      <div><?php the_field('motivation_desc'); ?></div>
+    </div>
+    <!-- Goals -->
+    <div>
+      <img src="<?php the_field('goals_icon'); ?>">
+      <h5><?php the_field('goals_title'); ?></h5>
+      <div><?php the_field('goals_desc'); ?></div>
+    </div>
+    <!-- Fustrations -->
+    <div>
+      <img src="<?php the_field('frustrations_icon'); ?>">
+      <h5><?php the_field('frustrations_title'); ?></h5>
+      <div><?php the_field('frustrations_desc'); ?></div>
+    </div>
+    <!-- Top Deciding Factors -->
+    <div>
+      <img src="<?php the_field('deciding_factors_icon'); ?>">
+      <h5><?php the_field('deciding_factors_title'); ?></h5>
+      <div class="green-bkg" style="width:<?php the_field('atmosphere_percent'); ?>;"><p class="white-font">Hello </p></div>
+      <div class="green-bkg" style="width:<?php the_field('price_percent'); ?>;"><p class="white-font">Hello </p></div>
+      <div class="green-bkg" style="width:<?php the_field('location_percent'); ?>;"><p class="white-font">Hello </p></div>
+      <div class="green-bkg" style="width:<?php the_field('good_reviews_percent'); ?>;"><p class="white-font">Hello </p></div>
+      <div class="green-bkg" style="width:<?php the_field('convenience_percent'); ?>;"><p class="white-font">Hello </p></div>
     </div>
   </div>
 </div>
+  <!-- ROW 3 -->
+  <div class="width-25 m-width-50 sm-width-100">
+    <div class="med-padding-left">
+    <div class="green-bkg">
+      <p><?php the_field('quote'); ?></p>
+    </div>
+    <div>
+      <!-- Words to Describe -->
+    <div>
+      <img src="<?php the_field('describe_words_icon'); ?>">
+      <h5><?php the_field('describe_words_title'); ?></h5>
+      <div><?php the_field('describe_words_desc'); ?></div>
+    </div>
+    <!-- Brands -->
+    <div>
+      <img src="<?php the_field('brand_icon'); ?>">
+      <h5><?php the_field('brand_title'); ?></h5>
+      <img src="<?php the_field('brand_images'); ?>">
+    </div>
+  </div>
+  </div>
+</div>
+  <!-- ROW 4 -->
+  <div class="width-25 m-width-50 sm-width-100">
+    <div class="med-padding-left">
+      <h2><?php the_field('persona_title'); ?></h2>
+      <p><?php the_field('persona_desc'); ?></p>
+    </div>
+  </div>
+</div>
+</div>
+  <div>
 </section>
-<!-- End of About section -->
-
-<!-- Skills section -->
+<!-- End of work flow slider -->
+<!-- Project Slider -->
 <section class="white-bkg lg-margin-bottom">
-<div class="xlg-padding">
+ <?php echo do_shortcode('[rev_slider alias="Explore_project"]'); ?>
+</section>
+<!-- End of Project Slider -->
+<!-- Take away section -->
+<section class="white-bkg lg-margin-bottom">
+<div class="project-padding">
   <div class="sm-padding">
   <div class="flex flex-wrap space-between">
-    <div class="width-32 ">
-     <h1><?php the_field('skills_title'); ?></h1>
+    <div class="width-32 m-width-100">
+     <img src="<?php the_field('take_away_image'); ?>">
    </div>
-   <div class="width-32 flex direction-column justify-center lg-padding-bottom">
-     <div class="lg-padding-right">
-     <div class="flex">
-       <img src="<?php the_field('skill_1_image'); ?>" class="width-auto object-cover med-padding-bottom">
-        <h2 class="med-padding-left"><?php the_field('skill_1_title'); ?></h2>
-      </div>
-        <p><?php the_field('skill_1_desc'); ?></p>
-      </div>
-   </div>
-   <div class="width-32 flex direction-column justify-center lg-padding-bottom">
-     <div class="lg-padding-right">
-    <div class="flex">
-     <img src="<?php the_field('skill_2_image'); ?>" class="width-auto object-cover med-padding-bottom">
-      <h2 class="med-padding-left"><?php the_field('skill_2_title'); ?></h2>
-   </div>
-     <p><?php the_field('skill_2_desc'); ?></p>
-   </div>
-   </div>
-   <div class="width-32 flex direction-column justify-center lg-padding-bottom">
-     <div class="lg-padding-right">
-     <div class="flex">
-     <img src="<?php the_field('skill_3_image'); ?>" class="width-auto object-cover med-padding-bottom">
-      <h2 class="med-padding-left"><?php the_field('skill_3_title'); ?></h2>
-   </div>
-      <p><?php the_field('skill_3_desc'); ?></p>
+    <div class="width-60 m-width-100">
+      <p class="bold-font"><?php the_field('take_away_title'); ?></p>
+      <p><?php the_field('take_away_desc'); ?></p>
     </div>
-   </div>
-   <div class="width-32 flex direction-column justify-center lg-padding-bottom">
-     <div class="lg-padding-right">
-    <div class="flex">
-     <img src="<?php the_field('skill_4_image'); ?>" class="width-auto object-cover med-padding-bottom">
-     <h2 class="med-padding-left"><?php the_field('skill_4_title'); ?></h2>
-   </div>
-     <p><?php the_field('skill_4_desc'); ?></p>
-   </div>
-   </div>
-   <div class="width-32 flex direction-column justify-center lg-padding-bottom">
-     <div class="lg-padding-right">
-     <div class="flex">
-     <img src="<?php the_field('skill_5_image'); ?>" class="width-auto object-cover med-padding-bottom">
-     <h2 class="med-padding-left"><?php the_field('skill_5_title'); ?></h2>
-   </div>
-     <p><?php the_field('skill_5_desc'); ?></p>
-   </div>
-   </div>
 </div>
 </div>
 </div>
 </section>
-<!-- End of Case study section -->
+<!-- End of Take away section -->
 
 <section class="white-bkg lg-margin-bottom">
 
-<section id="content" role="main">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-<?php if ( ! post_password_required() ) comments_template( '', true ); ?>
-<?php endwhile; endif; ?>
-</section>
 </section>
 <?php get_footer(); ?>
